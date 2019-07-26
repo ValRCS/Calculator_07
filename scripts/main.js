3fe816011001d3dd3ff847a558ee25c6934b0934
Valdis Saules <valdis.s.coding@gmail.com>
2019-07-26 12:38:01 +0300

    Added divs

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,13 @@
 console.log("Starting Calculator");
+
+const main = document.getElementById('App');
+main.innerHTML = '<p>Our elements will be here</p>';
+
+for (let i=0;i < 16;i++) {
+    const myDiv = document.createElement("div");
+    myDiv.innerText = "Some Text for Div # "+i;
+    myDiv.id = "d"+i;
+    myDiv.className = "btn";
+    myDiv.className += " numbers";
+    main.appendChild(myDiv);
+}
