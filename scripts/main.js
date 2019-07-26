954a5ec3d930be4337d26c6ac008769c936ff007
Valdis Saules <valdis.s.coding@gmail.com>
2019-07-26 13:01:17 +0300

    Added listeners

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@ function main() {
     console.log("Starting Calculator");
 
     const main = document.getElementById('App');
-    main.innerHTML = '<p>Our elements will be here</p>';
+    // main.innerHTML = '<p>Our elements will be here</p>';
 
     for (let i=0;i < 16;i++) {
         const myDiv = document.createElement("div");
@@ -10,8 +10,20 @@ function main() {
         myDiv.id = "d"+i;
         myDiv.className = "btn";
         myDiv.className += " numbers";
+        
         main.appendChild(myDiv);
+        myDiv.onmousedown = onMouseDown;
+        // myDiv.onmouseleave = () => {console.log("Leaving some Div")};
     }
+
+    // main.onmousemove = (ev) => {console.log("X:"+ev.pageX+"Y:"+ev.pageY)};
+    console.log("Created Main");
+}
+
+function onMouseDown(event) {
+    console.log("You pressed some div"+event.target.id);
+    
+    // console.log("Coordinates "+event.pageX+"Y:"+event.pageY);
 }
 
 main();
