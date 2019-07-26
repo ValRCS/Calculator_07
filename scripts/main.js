7c4bd51689d4fc7307a36bdeaafab48c816ea256
Valdis Saules <valdis.s.coding@gmail.com>
2019-07-26 12:41:05 +0300

    Refactored to use main function

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,18 @@
-console.log("Starting Calculator");
+function main() {
+    console.log("Starting Calculator");
 
-const main = document.getElementById('App');
-main.innerHTML = '<p>Our elements will be here</p>';
+    const main = document.getElementById('App');
+    main.innerHTML = '<p>Our elements will be here</p>';
 
-for (let i=0;i < 16;i++) {
-    const myDiv = document.createElement("div");
-    myDiv.innerText = "Some Text for Div # "+i;
-    myDiv.id = "d"+i;
-    myDiv.className = "btn";
-    myDiv.className += " numbers";
-    main.appendChild(myDiv);
+    for (let i=0;i < 16;i++) {
+        const myDiv = document.createElement("div");
+        myDiv.innerText = "Some Text for Div # "+i;
+        myDiv.id = "d"+i;
+        myDiv.className = "btn";
+        myDiv.className += " numbers";
+        main.appendChild(myDiv);
+    }
 }
+
+main();
+
